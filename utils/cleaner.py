d64f1bc512e5d5b9b1867f7f446b0bf12a46e786
slash <1364435561@qq.com>
2022-12-12 22:23:21 +0800

    update

--- utils/cleaner.py
+++ utils/cleaner.py
@@ -1,16 +1,17 @@
 import re
 import pandas as pd
 
+
 def clear(x):
-    x = re.sub('」「','」、「',x)
-    x = re.sub('\n|\t|\r| |​|※|〓|﻿|●|\d{8}|={1,}|…+|▌|」|『|』|「|>>(.*?)<<|”|“|• ','',str(x))
-    x = re.sub('！+','！',x)
-    x = re.sub('!+','!',x)
+    x = re.sub('」「', '」、「', x)
+    x = re.sub('\n|\t|\r| |​|※|〓|﻿|●|\d{8}|={1,}|…+|▌|」|『|』|「|>>(.*?)<<|”|“|• ', '', str(x))
+    x = re.sub('！+', '！', x)
+    x = re.sub('!+', '!', x)
     url_pattern = r'((?:(?<=[^a-zA-Z0-9]){0,}(?:(?:https?\:\/\/){0,1}(?:[a-zA-Z0-9\%]{1,}\:[a-zA-Z0-9\%]{1,}[@]){,1})(?:(?:\w{1,}\.{1}){1,5}(?:(?:[a-zA-Z]){1,})|(?:[a-zA-Z]{1,}\/[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\:[0-9]{1,4}){1})){1}(?:(?:(?:\/{0,1}(?:[a-zA-Z0-9\-\_\=\-]){1,})*)(?:[?][a-zA-Z0-9\=\%\&\_\-]{1,}){0,1})(?:\.(?:[a-zA-Z0-9]){0,}){0,1})'
-    x = re.sub(url_pattern,'',x)
-    x = re.sub('（|\(|【(.*?)】|\)|）','',x)
-    x = re.sub(' {2,}',' ',x)
-    x = re.sub('【|_|活动时间(.*?)59|','',x)
+    x = re.sub(url_pattern, '', x)
+    x = re.sub('（|\(|【(.*?)】|\)|）', '', x)
+    x = re.sub(' {2,}', ' ', x)
+    x = re.sub('【|_|活动时间(.*?)59|', '', x)
     if 'CV' in x:
         x = x[:x.index('CV')]
     if '>>>' in x:
@@ -19,59 +20,80 @@ def clear(x):
     # print(x)
 
 
-
 def clear_notice():
     df = pd.read_csv('../data/notice.csv')
-    df['s_content'] = df['s_content'].apply(lambda x:clear(''.join(eval(x))))
-    df.drop_duplicates(subset='s_content',inplace=True)
+    df['s_content'] = df['s_content'].apply(lambda x: clear(''.join(eval(x))))
+    df.drop_duplicates(subset='s_content', inplace=True)
     content = df['s_content'].tolist()
-    df['len'] = df['s_content'].apply(lambda x:len(x))
-    with open('../data/hmy-notice.txt','w') as f:
+    df['len'] = df['s_content'].apply(lambda x: len(x))
+    with open('../data/hmy-notice.txt', 'w') as f:
         for i in content:
 
-            i = i.replace('!','。').replace('！','。').replace('；','。').replace(';','。').replace('?','？')
-            res = [k for k in i.split('。') if len(k)>1]
-            if 0<len(res)<2:
-                if len(res[0])>30:
-                    f.write(res[0]+'\n')
+            i = i.replace('!', '。').replace('！', '。').replace('；', '。').replace(';', '。').replace('?', '？')
+            res = [k for k in i.split('。') if len(k) > 1]
+            if 0 < len(res) < 2:
+                if len(res[0]) > 30:
+                    f.write(res[0] + '\n')
             else:
-                for j in range(len(res)-1):
-                    s1 = res[j] if res[j].endswith('？') else res[j]+'。'
-                    s2 = res[j+1] if res[j+1].endswith('？') else res[j+1]+'。'
-                    s = (s1+s2).replace(' ','')
-                    if 256>len(s) >30:
-                        f.write(s+'\n')
+                for j in range(len(res) - 1):
+                    s1 = res[j] if res[j].endswith('？') else res[j] + '。'
+                    s2 = res[j + 1] if res[j + 1].endswith('？') else res[j + 1] + '。'
+                    s = (s1 + s2).replace(' ', '')
+                    if 256 > len(s) > 30:
+                        f.write(s + '\n')
         f.close()
     # print(df['len'].value_counts())
 
 
-
-
 def clear_info():
     res = []
-    with open('../data/mhy-info.txt','r') as f:
+    with open('../data/mhy-info.txt', 'r') as f:
         for line in f.readlines():
-
             while '」「' in line:
                 idx = line.find('」「')
-                if re.findall('[\u4e00-\u9fa5\da-zA-Z]+',line[idx-1]) and re.findall('[\u4e00-\u9fa5\da-zA-Z]+',line[idx+2]):
-                    line = line.replace('」「','，')
+                if re.findall('[\u4e00-\u9fa5\da-zA-Z]+', line[idx - 1]) and re.findall('[\u4e00-\u9fa5\da-zA-Z]+',
+                                                                                        line[idx + 2]):
+                    line = line.replace('」「', '，')
                 else:
-                    line = line.replace('」「','')
-            line = re.sub('」|「|『|』','',line)
+                    line = line.replace('」「', '')
+            line = re.sub('」|「|『|』', '', line)
             print(line)
-            if len(line)>15:
+            if len(line) > 15:
                 res.append(line)
-    with open('../data/mhy-info-clear.txt','w') as f1:
+    with open('../data/mhy-info-clear.txt', 'w') as f1:
         [f1.write(i) for i in res]
 
 
-
-
+def clear_audio():
+    res = []
+    symbol = set()
+    rep_dict ={'~！':'~','···？':'？','….':'...','~~':'~','&nbsp;':'','…':'...',' ':'',',':'，',' ':'','██':'',
+               '···':'...','？！':'？','！。':'！'}
+    with open('../dataset/mhy-audio.txt') as f:
+        for line in f.readlines():
+            while '」「' in line:
+                idx = line.find('」「')
+                if re.findall('[\u4e00-\u9fa5\da-zA-Z]+', line[idx - 1]) and re.findall('[\u4e00-\u9fa5\da-zA-Z]+',
+                                                                                        line[idx + 2]):
+                    line = line.replace('」「', '，')
+                else:
+                    line = line.replace('」「', '')
+            line = re.sub('」|「|『|』', '', line)
+            if len(re.findall('[\u4e00-\u9fa5]', line)) >= 20 and '特殊料理' not in line:
+                for k,v in rep_dict.items():
+                    line = line.replace(k,v)
+                [symbol.add(j) for j in re.findall('[^\u4e00-\u9fa5\da-zA-Z]+',line)]
+                res.append(line)
+    with open('../data/mhy-audio-clear.txt', 'w') as f1:
+        [f1.write(i) for i in res]
+    print(symbol)
 
 if __name__ == '__main__':
-    res = []
-    with open('../data/mhy-notice.txt') as f:
-        for i in f.readlines():
-            res.extend(re.findall('[^\u4e00-\u9fa5\da-zA-Z!@#$%^&*()_+?><":：？》《【】\[\]（）！、;；]+',i.replace('\n','')))
-    print(set(res))
+    # clear_audio()
+    max_len = 0
+    with open('../data/mhy-audio-clear.txt', 'r') as f1:
+        for line in f1.readlines():
+            max_len = max(max_len,len(line.replace('\n','')))
+            if len(line.replace('\n',''))>256:
+                print(line)
+    print(max_len)
