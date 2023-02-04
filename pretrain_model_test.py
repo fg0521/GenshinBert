cd1a91e6be1f5edf057e1d76c42babb54d43363e
fg0521 <1364435561@qq.com>
2023-02-04 12:38:54 +0800

    update

--- pretrain_model_test.py
+++ pretrain_model_test.py
@@ -1,3 +1,5 @@
+import copy
+
 from transformers import RobertaTokenizer, RobertaForMaskedLM,BertTokenizer,BertForMaskedLM
 import torch
 
@@ -5,8 +7,8 @@ import torch
 model_path = 'genshin-bert'
 tokenizer = BertTokenizer.from_pretrained(model_path)
 model = BertForMaskedLM.from_pretrained(model_path)
-model_org = BertForMaskedLM.from_pretrained('/Users/maoyufeng/slash/models/chinese-bert-wwm-ext')
-text = '原[MASK]游戏中有一个城市叫做[MASK]月，里面有一位岩神，他的名字叫做[MASK]拉克斯，也叫做钟[MASK]。他曾经封印了[MASK]陀龙王'
+model_org = BertForMaskedLM.from_pretrained('/Users/maoyufeng/slash/pre_trained_models/chinese-bert-wwm-ext')
+text = '那太好了，那就麻烦珊瑚[MASK]心[MASK]大人啦。'
 text_tokens = tokenizer(text, add_special_tokens=True,
                              padding=True, return_tensors='pt')
 
@@ -21,10 +23,22 @@ pred = pred.data.cpu().numpy().tolist()[0]
 
 pred1 = torch.argmax(outputs1.logits, dim=-1).cpu().numpy().tolist()[0]
 
-pred_tokens =tokenizer.decode(pred[1:-1])
-pred_tokens1 =tokenizer.decode(pred1)
 
-
+s = copy.deepcopy(text)
+# pos = []
+while '[MASK]' in s:
+    p = s.find('[MASK]')
+    if p != -1:
+        # pos.append(p+1)
+        pred_word = tokenizer.decode(pred[p+1])
+        s = s.replace('[MASK]',pred_word,1)
 print(f"原始句子：{text}")
-print(f"预测句子：{pred_tokens.replace(' ','')}")
-print(f"预测句子1：{pred_tokens1.replace(' ','')}")
+print(f"预测句子：{s}")
+
+
+# pred_tokens =tokenizer.decode(pred[1:-1])
+# pred_tokens1 =tokenizer.decode(pred1)
+# print(tokenizer.decode(pred[11]))
+
+# print(f"预测句子：{pred_tokens.replace(' ','')}")
+# print(f"预测句子1：{pred_tokens1.replace(' ','')}")
